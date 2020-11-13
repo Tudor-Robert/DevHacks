ddaab4279331c1681fd4919284d78a5f5777302b
mana5861 <madalina.mariana11@yahoo.com>
2020-11-13 15:51:55 +0200

    Initial commit

---
+++ src/main/java/com/DevHacks/Initializer.java
@@ -0,0 +1,20 @@
+package com.DevHacks;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class Initializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{Config.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{Config.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
---
+++ src/main/webapp/WEB-INF/display.jsp
@@ -0,0 +1,18 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: madal
+  Date: 31.08.2020
+  Time: 13:07
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" isELIgnored="false" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+Result is: ${result}
+<br>
+Datele au fost salvate
+</body>
+</html>
